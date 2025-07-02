51f0dd7af3b528b7f0df26199e78727f9fbcd037
cygnusA <bboong2025@gmail.com>
2025-07-02 19:40:57 +0900

    display items

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -4,11 +4,27 @@ function loadItems() {
     .then(response => response.json())
     .then(json => console.items);
 }
+//Update the list
+function displayItems(items) {
+    const container = document.querySelector('.items');
+    const html = items.map(item => createHTMLString(item));
+    console.log(html);
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
+
+//create HTML
+function createHTMLString(item) {
+    return`
+    <li class="item">
+        <img src="${item.image} alt="${item.type}" class="item_thumbnail" />
+        <span class="item_description">${item.gender},${item.size}</span>
+    </li>
+    `;
+}
 
 loadItems()
-.then(items => {
-    console.log(items);
-    // displayItems(items);
-    // setEvelntLIstener(items);
+    .then(items => {
+    displayItems(items);
+    //setEvelntLIstener(items);
 })
 .catch(console.log)
