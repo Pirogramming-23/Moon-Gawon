ae45b5a142b324fea2c111ed1553c6b6d102d0d8
cygnusA <bboong2025@gmail.com>
2025-07-02 23:53:33 +0900

    초기화 로직구현

--- 숫자야구 게임/script.js
+++ 숫자야구 게임/script.js
@@ -6,9 +6,21 @@ function check_numbers(){
     const number1 = document.getElementById("number1").value;
     const number2 = document.getElementById("number2").value;
     const number3 = document.getElementById("number3").value;
-    console.log(number1,number2,number3);
+
+    document.getElementById("number1").value ="";
+    document.getElementById("number2").value ="";
+    document.getElementById("number3").value ="";
     let strike = 0;
     let ball = 0;
+
+    //TODO: input값이 비워져 있을떄 초기화
+    if (!number1 || !number2 || !number3){
+        console.log("초기화");
+        document.getElementById("number1").value ="";
+        document.getElementById("number2").value ="";
+        document.getElementById("number3").value ="";
+        return;
+    }
 
     //TODO: 결과값을 만들어서 ball인지, strike인지 출력
     //TODO: answer와 input값 비교하기
@@ -23,6 +35,18 @@ function check_numbers(){
             ball++;
         }
     }
+    let result =`${input.join("")}=`;
+    if(strike===0 && ball===0){
+        result+=`0`;
+    }
+    else if(strike>0){
+        result+=`${strike}`;
+    }
+    else if(ball>0){
+        result+=`${ball}`;
+    }
+    const results= document.getElementById("results");
+    results.innerHTML += result+ `<br>`;
 
 
 }
