97d48cc55333bc88aa26c8671c9857df3f413bdf
cygnusA <bboong2025@gmail.com>
2025-07-02 23:25:32 +0900

    숫자 비교 로직 구현

---
+++ 숫자야구 게임/script.js
@@ -0,0 +1,28 @@
+// @ts-nocheck
+//TODO: 숫자 비교 스트라이크 볼 처리
+
+const answer=["1","2","3"];
+function check_numbers(){
+    const number1 = document.getElementById("number1").value;
+    const number2 = document.getElementById("number2").value;
+    const number3 = document.getElementById("number3").value;
+    console.log(number1,number2,number3);
+    let strike = 0;
+    let ball = 0;
+
+    //TODO: 결과값을 만들어서 ball인지, strike인지 출력
+    //TODO: answer와 input값 비교하기
+    const input=[number1,number2,number3];
+    
+    for(let i=0;i<3;i++){
+        console.log(`비교 중: answer[${i}] = ${answer[i]}, input[${i}] = ${input[i]}`);
+        if(answer[i]===input[i]){
+            strike++;
+        }
+        else if(answer.includes(input[i])){
+            ball++;
+        }
+    }
+
+
+}
