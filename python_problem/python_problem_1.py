5787279cca831d0c06a3e6b15ae90d1ce08e3211
cygnusA <bboong2025@gmail.com>
2025-07-04 14:44:57 +0900

    5단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,9 +1,13 @@
 # 1단계: 변수 선언
 num = 0
+players =["playerA", "playerB"]
+turn = 0
 # 2단계: input() 값으로 정수입력받기
 
 # 3단계: if문을 사용하여 올바른지 확인
 while True:
+    print(f"\n{players[turn]}의 차례입니다.")
+
     while True:
         a = int(input("정수를 입력하세요: "))
         if a <= 0:
@@ -17,4 +21,6 @@ while True:
     #4단계: 입력받은 값
     for _ in range(a):
         num += 1
-        print(f"playerA: {num}")
+        print(f"{players[turn]}: {num}")
+    turn = (num - 1) % 2
+    # 5단계: 입력받은 값
