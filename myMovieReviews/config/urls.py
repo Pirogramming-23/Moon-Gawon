8342e223cd432c73e9c42f94f47346d902f0e0fd
Moon-gawon <bboong2025@gmail.com>
2025-07-11 16:34:46 +0900

    이미지 업로드 기능 추가

--- myMovieReviews/config/urls.py
+++ myMovieReviews/config/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('review_list', permanent=False)),
     path('review/', include("review.urls")),
     path('admin/', admin.site.urls),
 ]
